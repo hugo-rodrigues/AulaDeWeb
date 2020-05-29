f15e38880e20ff5bad653059ac9450ca5cffdfe2
Hugo Rodrigues <hugo.hen.rodrigues@gmail.com>
2020-05-28 21:39:45 -0300

    Exercicio 2 de js

---
+++ Exercicio JS 2/exercicio2.js
@@ -0,0 +1,52 @@
+var receita = {
+    titulo: 'teste',
+    porcao: 1,
+    ingredientes: ['valor 1', 'valor 2']
+}
+
+
+console.log(receita.titulo, receita.porcao, receita.ingredientes)
+
+var livrostotal = []
+
+var Livros1 = {
+    titulo: 'teste1',
+    autor: 'teste1',
+    livroLido: true
+}
+
+var Livros2 = {
+    titulo: 'teste2',
+    autor: 'teste2',
+    livroLido: false
+}
+
+var Livros3 = {
+    titulo: 'teste3',
+    autor: 'teste3',
+    livroLido: false
+}
+
+var Livros4 = {
+    titulo: 'teste4',
+    autor: 'teste4',
+    livroLido: true
+}
+
+
+livrostotal[0] = Livros1
+
+livrostotal[1] = Livros2
+
+livrostotal[2] = Livros3
+
+livrostotal[3] = Livros4
+
+for (i = 0; livrostotal.length; i++) {
+    console.log(livrostotal[i].titulo, livrostotal[i].autor)
+    if (livrostotal[i].livroLido == true) {
+        console.log("Livro Lido com sucesso")
+    } else {
+        console.log("Livro nao Lido com sucesso")
+    }
+}
